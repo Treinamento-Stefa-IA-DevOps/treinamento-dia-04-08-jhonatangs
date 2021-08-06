a3f49c2bdc508a9239c715b1cb840fe7c90f8426
jhonatangs <jhonatangs1@gmail.com>
2021-08-06 18:48:44 -0300

    api fixed

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,46 @@
 import pickle
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Person(BaseModel):
+    Sex: int
+    Age: float
+    Lifeboat: int
+    Pclass: int
+
 
 app = FastAPI()
-@app.post('/model')
-## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
-        titanic = pickle.load(fid)
 
-@app.get('/model')
+
+@app.post("/model")
+## Coloque seu codigo na função abaixo
+def titanic(person: Person):
+    with open("model/Titanic.pkl", "rb") as fid:
+        try:
+            titanic = pickle.load(fid)
+            y_pred = bool(
+                titanic.predict(
+                    [[person.Sex, person.Age, person.Lifeboat, person.Pclass]]
+                )[0]
+            )
+
+            return {
+                "survived": y_pred,
+                "status": 200,
+                "message": "Survived" if y_pred else "Did not survive",
+            }
+
+        except Exception as e:
+            return {"survived": None, "status": 500, "message": e}
+
+
+@app.get("/model")
 def get():
-    return {'hello':'test'}
+    return {"hello": "test"}
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello, Titanic!"}
